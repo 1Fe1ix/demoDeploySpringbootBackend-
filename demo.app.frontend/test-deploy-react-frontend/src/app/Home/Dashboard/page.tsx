48387f9dd8b91c79ab940ba14e4da3ef5866e798
Felix Hoffmann <hoffmannfelix.3@gmail.com>
2025-07-12 20:37:55 +0200

    added frontend and dependencies

---
+++ demo.app.frontend/test-deploy-react-frontend/src/app/Home/Dashboard/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+function App() {
+  return (
+    <Box
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      height='100vh'
+    >
+      <Typography variant='h3'>Hallo Welt</Typography>
+    </Box>
+  )
+}
+
+export default App
